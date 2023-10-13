1ab5c96ff8b19cacabd829c6af7c9a86ee9c07ba
Rio Edwards <rioredwards@gmail.com>
2023-10-13 00:41:23 -0700

    Start filling out project structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="python-wrapper-for-contentful-rich-text-to-markdown-converter",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            # any command-line utilities you want to provide
+        ],
+    },
+)
