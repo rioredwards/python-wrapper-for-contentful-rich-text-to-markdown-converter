63335b2a942763e6b826e0f07816d7ded981e385
Rio Edwards <rioredwards@gmail.com>
2023-10-13 01:58:46 -0700

    Setup setup.py in prep to publish on PyPI

--- setup.py
+++ setup.py
@@ -3,8 +3,25 @@ from setuptools import setup, find_packages
 setup(
     name="python-wrapper-for-contentful-rich-text-to-markdown-converter",
     version="0.1",
+    description="A Python wrapper for converting Contentful rich text to Markdown",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rioredwards/python-wrapper-for-contentful-rich-text-to-markdown-converter",
+    author="Rio Edwards",
+    author_email="rioredwards@gmail.com",
+    license="MIT",
     packages=find_packages(),
     install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     entry_points={
         "console_scripts": [
             # any command-line utilities you want to provide
